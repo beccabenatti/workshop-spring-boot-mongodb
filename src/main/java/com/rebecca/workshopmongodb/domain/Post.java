e6e97afc810fcdf453bc1b2ec00d0eeb9173c5ab
Rebecca Benatti <rebeccardgsilva@gmail.com>
2022-07-22 16:25:58 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/rebecca/workshopmongodb/domain/Post.java
+++ src/main/java/com/rebecca/workshopmongodb/domain/Post.java
@@ -1,13 +1,16 @@
 package com.rebecca.workshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.rebecca.workshopmongodb.dto.AuthorDTO;
+import com.rebecca.workshopmongodb.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -18,7 +21,9 @@ public class Post implements Serializable{
 	private Date date;
 	private String title;
 	private String body;
-	private AuthorDTO author; 
+	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -72,6 +77,15 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 
 	@Override
 	public int hashCode() {
